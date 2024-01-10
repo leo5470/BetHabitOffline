424b2d8c23c618355854a231b58e7cb7a406db73
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-01-10 21:58:18 +0800

    Create DetailedHabitView.swift

---
+++ BetHabitOffline/Views/DetailedHabitView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailedHabitView.swift
+//  BetHabitOffline
+//
+//  Created by Leo Cheng on 2024/1/9.
+//
+
+import SwiftUI
+
+struct DetailedHabitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailedHabitView()
+}
