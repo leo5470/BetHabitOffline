3bd582a30a2dd6be36d1c745774aaba025b6f0df
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-01-10 21:57:48 +0800

    Complete basic ContentView

--- BetHabitOffline/ContentView.swift
+++ BetHabitOffline/ContentView.swift
@@ -10,7 +10,65 @@ import SwiftData
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ScrollView {
+            VStack {
+                Spacer()
+                    .frame(height: 20)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(getTodayString())
+                            .font(.system(size: 14))
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.black.opacity(0.55))
+                        
+                        Text("Today")
+                            .font(.system(size: 40))
+                            .fontWeight(.bold)
+                    }
+                    
+                    Spacer()
+                    
+                    Image(systemName: "list.bullet.circle.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .foregroundStyle(.accent)
+                }
+                .padding([.leading, .trailing], 20.0)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                
+                HStack {
+                    Text("Not Checked")
+                        .font(.system(size: 24))
+                        .fontWeight(.semibold)
+                    .foregroundStyle(.black)
+                    
+                    Spacer()
+                }
+                .padding([.leading, .trailing], 20.0)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                // TODO: multiple views
+                HStack {
+                    HabitCardView(habit: getDummyHabit())
+                }
+                
+                Spacer()
+                    .frame(height: 30)
+                
+                HStack {
+                    Text("Checked")
+                        .font(.system(size: 24))
+                        .fontWeight(.semibold)
+                    .foregroundStyle(.black)
+                }
+                .padding([.leading, .trailing], 20.0)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                // TODO: Multiple views
+                HStack {
+                    HabitCardView(habit: getDummyHabit())
+                }
+            }
+        }
     }
 }
 
