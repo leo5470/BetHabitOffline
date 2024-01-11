a90b0fb8f22f9258f40f77f461d8bfab97e11eac
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-01-11 11:08:41 +0800

    Add getDummyHabitArray()

--- BetHabitOffline/Resources/DummyHabit.swift
+++ BetHabitOffline/Resources/DummyHabit.swift
@@ -17,3 +17,19 @@ func getDummyHabit() -> Habit {
     
     return dummyHabit
 }
+
+func getDummyHabitArray() -> [Habit] {
+    let dummyPeopleInfo: [Person] = [Person(
+        name: "Bamboo",
+        bet: "A cup of Long Island")]
+
+    let dummyHabit: Habit = Habit(name: "Study hard",start: Date(timeIntervalSinceNow: -86400), deadline: Date(timeIntervalSinceNow: 432000), peopleNum: 1, peopleInfo: dummyPeopleInfo)
+    
+    var dummyArray: [Habit] = []
+    
+    for _ in 1...10 {
+        dummyArray.append(dummyHabit)
+    }
+    
+    return dummyArray
+}
