18c8d82d447023314a314fd31c639eec4578a4a6
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-03-05 16:57:45 +0800

    Update HabitModel

--- BetHabitOffline/Models/HabitModel.swift
+++ BetHabitOffline/Models/HabitModel.swift
@@ -11,11 +11,28 @@ import SwiftData
 struct Person : Codable {
     var name: String
     var bet: String
+    
+    init() {
+        self.name = ""
+        self.bet = ""
+    }
+    
+    init(name: String, bet: String) {
+        self.name = name
+        self.bet = bet
+    }
 }
 
-//@Model
-class Habit : Identifiable, ObservableObject {
-    var id: String
+struct HabitPrototype: Hashable {
+    var name: String
+    var deadline: Date
+    var peopleNum: Int
+}
+
+@Model
+class Habit : Identifiable, Hashable, ObservableObject {
+    
+    @Attribute(.unique) let id: String = UUID().uuidString
     var name: String
     var start: Date
     var deadline: Date
@@ -24,9 +41,61 @@ class Habit : Identifiable, ObservableObject {
     var peopleNum: Int
     var peopleInfo: [Person]
     var dailyChecked: Bool
+    var failed: Bool
+    
+    static func == (lhs: Habit, rhs: Habit) -> Bool { // Hashable conforms to Equatable
+        return lhs.id == rhs.id // Since id is in UUID, the two ids must be unique
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id) // Since id is in UUID, the two ids must be unique
+    }
+    
+    init() {
+        self.name = ""
+        // Swift does not allow usage of var without completely initializing
+        let now: Date = Date.now
+        self.start = now
+        self.deadline = now
+        self.passedDays = 0
+        self.peopleNum = 0
+        self.peopleInfo = []
+        self.dailyChecked = false
+        self.totalDays = 0
+        self.failed = false
+    }
+    
+    init(habitPrototype: HabitPrototype) {
+        self.name = habitPrototype.name
+        // Swift does not allow usage of var without completely initializing
+        let startCopy: Date = Date.now
+        self.start = startCopy
+        
+        self.deadline = habitPrototype.deadline
+        self.passedDays = 0
+        self.peopleNum = habitPrototype.peopleNum
+        self.peopleInfo = Array(repeating: Person(), count: habitPrototype.peopleNum)
+        self.dailyChecked = false
+        self.totalDays = calcDaysDiff(startCopy, habitPrototype.deadline)
+        self.failed = false
+    }
+    
+    init(name: String, deadline: Date, peopleNum: Int) {
+        self.name = name
+        // Swift does not allow usage of var without completely initializing
+        let startCopy: Date = Date.now
+        self.start = startCopy
+        
+        self.deadline = deadline
+        self.passedDays = 0
+        self.peopleNum = peopleNum
+        self.peopleInfo = Array(repeating: Person(), count: peopleNum)
+        self.dailyChecked = false
+        self.totalDays = calcDaysDiff(startCopy, deadline)
+        self.failed = false
+    }
     
     init(name: String, deadline: Date, peopleNum: Int, peopleInfo: [Person]) {
-        self.id = UUID().uuidString
         self.name = name
         // Swift does not allow usage of var without completely initializing
         let startCopy: Date = Date.now
@@ -39,10 +108,10 @@ class Habit : Identifiable, ObservableObject {
         self.dailyChecked = false
         
         self.totalDays = calcDaysDiff(startCopy, deadline)
+        self.failed = false
     }
     
     init(name: String, start: Date, deadline: Date, peopleNum: Int, peopleInfo: [Person]) {
-        self.id = UUID().uuidString
         self.name = name
         self.start = start
         
@@ -53,5 +122,14 @@ class Habit : Identifiable, ObservableObject {
         self.dailyChecked = false
         
         self.totalDays = calcDaysDiff(start, deadline)
+        self.failed = false
+    }
+    
+    func copyFromPrototype(habitPrototype: HabitPrototype){
+        name = habitPrototype.name
+        deadline = habitPrototype.deadline
+        peopleNum = habitPrototype.peopleNum
+        peopleInfo = Array(repeating: Person(), count: peopleNum)
+        totalDays = calcDaysDiff(start, deadline)
     }
 }
