9e22f11bd91bf9a5eae3ed199443104aba04067e
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-03-05 16:57:30 +0800

    Update dummy habit functionality

--- BetHabitOffline/Resources/DummyHabit.swift
+++ BetHabitOffline/Resources/DummyHabit.swift
@@ -10,9 +10,30 @@ import Foundation
 func getDummyHabit() -> Habit {
     let dummyPeopleInfo: [Person] = [Person(
         name: "Bamboo",
-        bet: "A cup of Long Island")]
+        bet: "A cup of Long Island"), Person(
+            name: "Bamboo",
+            bet: "A cup of Long Island")]
 
-    let dummyHabit: Habit = Habit(name: "Study hard",start: Date(timeIntervalSinceNow: -86400), deadline: Date(timeIntervalSinceNow: 432000), peopleNum: 1, peopleInfo: dummyPeopleInfo)
+    let dummyHabit: Habit = Habit(name: "Study hard",start: Date(timeIntervalSinceNow: -86400), deadline: Date(timeIntervalSinceNow: 432000), peopleNum: 2, peopleInfo: dummyPeopleInfo)
+    dummyHabit.passedDays += 1
+    
+    return dummyHabit
+}
+
+func getDummyHabit(_ checked: Bool) -> Habit {
+    let dummyPeopleInfo: [Person] = [Person(
+        name: "Bamboo",
+        bet: "A cup of Long Island"), Person(
+            name: "Bamboo",
+            bet: "A cup of Long Island"), Person(
+                name: "Bamboo",
+                bet: "A cup of Long Island")]
+
+    let dummyHabit: Habit = Habit(name: "Study hard",start: Date(timeIntervalSinceNow: -86400), deadline: Date(timeIntervalSinceNow: 432000), peopleNum: 3, peopleInfo: dummyPeopleInfo)
+    
+    if(checked){
+        dummyHabit.dailyChecked = true
+    }
     dummyHabit.passedDays += 1
     
     return dummyHabit
@@ -22,12 +43,11 @@ func getDummyHabitArray() -> [Habit] {
     let dummyPeopleInfo: [Person] = [Person(
         name: "Bamboo",
         bet: "A cup of Long Island")]
-
-    let dummyHabit: Habit = Habit(name: "Study hard",start: Date(timeIntervalSinceNow: -86400), deadline: Date(timeIntervalSinceNow: 432000), peopleNum: 1, peopleInfo: dummyPeopleInfo)
     
     var dummyArray: [Habit] = []
     
     for _ in 1...10 {
+        let dummyHabit: Habit = Habit(name: "Study hard",start: Date(timeIntervalSinceNow: -86400), deadline: Date(timeIntervalSinceNow: 432000), peopleNum: 1, peopleInfo: dummyPeopleInfo)
         dummyArray.append(dummyHabit)
     }
     
